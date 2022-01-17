b4b331b1d89bd97ff45032aafefb61850bfffa2f
saksham37 <sharma.ibn7@gmail.com>
2022-01-17 21:10:42 +0530

    show posts on home page

--- controllers/home_controllers.js
+++ controllers/home_controllers.js
@@ -1,9 +1,28 @@
+const Post = require('../models/post');
+
 module.exports.home = function(req, res){
-    console.log(req.cookies);
-    res.cookie('user_id', 25);
-    return res.render('home', {
-        title: "Home"
-    });
+    
+    // Post.find({},(err,posts)=>{
+    //     if(err){console.log("Unable to load the posts from the database "); return;}
+    //     return res.render('home',{
+    //         title:"Home",
+    //         posts
+    //     });
+    // })
+    
+    //Pre-population means, if there is any referenced object in the document, all its information will be loaded for every document
+    //After pre-population the post.user contains the entire info about the user instead of just the referenced object Id
+    Post.find({}).populate('user').exec((err,posts)=>{
+        if(err){console.log("unable to load posts from the database"); return;}
+        return res.render('home',{
+            title: "Home",
+            posts
+        });
+    })
+    
+    // return res.render('home', {
+    //     title: "Home"
+    // });
 }
 
 // module.exports.actionName = function(req, res){}

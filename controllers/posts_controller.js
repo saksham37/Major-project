73962eb212ffcbc7b4670ff68b0a79a02628f9c0
saksham37 <sharma.ibn7@gmail.com>
2022-01-19 21:20:57 +0530

    add post delete button

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,5 +1,5 @@
-const Post = require('../models/post')
-
+const Post = require('../models/post');
+const Comment = require('../models/comment');
 module.exports.create = function(req, res){
     Post.create({
         content: req.body.content,
@@ -9,4 +9,25 @@ module.exports.create = function(req, res){
 
         return res.redirect('back');
     });
+}
+//Deleting a post in the database
+module.exports.destroy = function(req,res){
+      //Post ki id se dhundo post ko
+      console.log("delete post controller called ");
+      console.log(req.params);
+      console.log(req.user.id);
+      Post.findById(req.params.id,function(err,post){
+          if(err){console.log("Error in finding the post in the database"); return;}
+            console.log("found the post");
+          //.id means converting the objectId into string
+          console.log(post.id);
+          if(post.user==req.user.id){
+              console.log("id's matched !!");
+             post.remove();//automatic function provided by mongoose to delete a document
+             //Now we need to delete all the comments of this particular post
+              Comment.deleteMany({post:req.params.id},(err)=>{
+                  res.redirect('back');
+              });
+          }
+      })
 }
--- routes/posts.js
+++ routes/posts.js
@@ -6,5 +6,8 @@ const postsController = require('../controllers/posts_controller');
 
 router.post('/create',passport.checkAuthentication, postsController.create);
 
+//user should be logged in to delete a post, that is the reason why we have added authentication here
+router.get('/destroy/:id',passport.checkAuthentication, postsController.destroy);
+//we will create a button for deletion of a post which will map to this router, giving us the id of the past to be deleted in the query params
 
 module.exports = router;
